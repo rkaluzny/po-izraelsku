00bbc619a40910e86260394b4f5915852139c721
rkaluzny <robin.jerome.kaluzny@gmail.com>
2025-12-23 11:52:28 +0100

    updated settings

--- articel/settings.py
+++ articel/settings.py
@@ -85,10 +85,9 @@ WSGI_APPLICATION = 'articel.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
