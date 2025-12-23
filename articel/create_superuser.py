5d1e5aac71f3415464b529f95e08ccda1a64994f
rkaluzny <robin.jerome.kaluzny@gmail.com>
2025-12-23 13:03:26 +0100

    create_superuser.py

---
+++ articel/create_superuser.py
@@ -0,0 +1,20 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "articel.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin"
+    )
+    print("Superuser created!")
+else:
+    print("Superuser already exists.")
